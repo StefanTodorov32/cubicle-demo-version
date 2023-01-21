4cc3bbb11cf1e888bc9d0b9f25d2c373c8565afa
StefanTodorov32 <bgtrak@gmail.com>
2023-01-21 20:52:12 +0200

    Search functionality fixed

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.use(detailesController)
 
 
 app.all('*', notFound)
-app.listen(3002, () => console.log("Server is running ..."));
+app.listen(3002, () => console.log("Server is running on http://localhost:3002..."));
