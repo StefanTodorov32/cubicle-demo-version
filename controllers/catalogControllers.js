8691084cf2d76d05956e77948b6048f572abdefa
StefanTodorov32 <bgtrak@gmail.com>
2023-01-27 11:00:28 +0200

    updated catalog

--- controllers/catalogControllers.js
+++ controllers/catalogControllers.js
@@ -2,21 +2,18 @@ const { getCubes, getSearchCube } = require("../services/cobicleService");
 
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  const cubes = getCubes();
-  res.render("catalog", {cubes});
-});
+router.get("/", getCubes);
 
 
 
 router.post('/', (req, res)=>{
-  const {search, from , to} = req.body
-  const cubes = getSearchCube(search, from , to)
-  if (cubes.length == 0) {
-    res.render("404")
-  }else{
-    res.render("catalog", {cubes});
-  }
+  // const {search, from , to} = req.body
+  // const cubes = getSearchCube(search, from , to)
+  // if (cubes.length == 0) {
+  //   res.render("404")
+  // }else{
+  //   res.render("catalog", {cubes});
+  // }
 
 })
 module.exports = router;
